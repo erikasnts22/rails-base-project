33fe1c76453050c6ad3ad083d852fd7fba54b32e
erikasnts22 <sntserika22@gmail.com>
2021-08-19 18:59:01 +0800

    nothing and junk error handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   #get 'home/index'
+
+  #for home page
   root 'home#index'
+
+  #for about page
   get 'home/about'
+
+  # for search field
+  #"/" = root URL pointing to 'home#index' controller
+  post "/" => 'home#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
