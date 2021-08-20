5dbb24bff75e8c4ba2e8c99ce39955ae164c1612
erikasnts22 <sntserika22@gmail.com>
2021-08-20 14:52:23 +0800

    modify user controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,8 @@
 class StocksController < ApplicationController
   before_action :set_stock, only: %i[ show edit update destroy ]
-
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
+  
   # GET /stocks or /stocks.json
   def index
     @stocks = Stock.all
@@ -54,6 +56,11 @@ class StocksController < ApplicationController
       format.html { redirect_to stocks_url, notice: "Stock was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  def correct_user
+    @ticker = current_user.stocks.find_by(id: params[:id])
+    redirect_to stocks_path, notice: "Not authorized to edit this stock." if @ticker.nil?
   end
 
   private
