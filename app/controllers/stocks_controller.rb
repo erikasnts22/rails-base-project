b03926ce24a6fcf3c5f25ac0c4d59ff348350e37
erikasnts22 <sntserika22@gmail.com>
2021-08-20 14:55:58 +0800

    add correct user controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,6 @@
 class StocksController < ApplicationController
   before_action :set_stock, only: %i[ show edit update destroy ]
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: %i[ edit update destroy]
   before_action :authenticate_user!
   
   # GET /stocks or /stocks.json
