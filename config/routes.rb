f9d6138cbb20f8d6e0cd63511f74aae48d08d6f8
erikasnts22 <sntserika22@gmail.com>
2021-08-20 09:18:32 +0800

    edit scaffold stylesheet

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   #get 'home/index'
 
-  #for home page
+  # home page
   root 'home#index'
 
-  #for about page
+  # about page
   get 'home/about'
 
-  # for search field
+  # search query
   #"/" = root URL pointing to 'home#index' controller
   post "/" => 'home#index'
 
